4292630f8b49e9b1d2aaec2612847060416df94a
powersjt4 <powersjt4@gmail.com>
2018-12-03 19:46:37 -0500

    Fixed up DMQ

--- proj/JPowers_DMQ.sql
+++ proj/JPowers_DMQ.sql
@@ -3,67 +3,58 @@ Jacob Powers
 DMQ Data Manipulation Queries For Project
 10/21/18
 */
+/*Add to Menu Page*/
+SELECT * FROM meal 
+SELECT menu.id, menu.restaurant_name, meal.name AS menu_meal FROM menu INNER JOIN meal ON menu.menu_meal = meal.id
+INSERT INTO menu(`restaurant_name`,`menu_meal`) VALUES (:restaurantName,:menuMealID)
+SELECT menu.id, menu.restaurant_name, meal.name AS menu_meal FROM menu INNER JOIN meal ON menu.menu_meal = meal.id WHERE menu.restaurant_name=:idFromJSFrontend
+DELETE FROM menu WHERE id=:idFromDeleteFunction
+SELECT menu.id, menu.restaurant_name, meal.name AS menu_meal, meal.id AS meal_id FROM menu INNER JOIN meal ON menu.menu_meal = meal.id WHERE menu.id=:internalSelectFunction
+UPDATE menu SET  restaurant_name = :nameFromTable , menu_meal = menuMealIDFromTable WHERE id = :idUpdateFunction;
+
 
 
 /*Add to item Page*/
 -- Propogate item table diplay select from item table and join pi and meal
-SELECT name, price, description FROM item 
-LEFT JOIN meals_table ON item.item_meal = meals_table.id  
-LEFT JOIN pi_table ON item.primary_ingredient = primary_ingredient 
+SELECT * FROM meal
+SELECT * FROM primary_ingredient //For dropdown
+/*Propogates items table*/
+SELECT i.id, i.name, i.price, i.description, m.name AS item_meal, pi.name AS primary_ingredient FROM item i 
+INNER JOIN meal m ON i.item_meal = m.id INNER JOIN primary_ingredient pi ON i.primary_ingredient = pi.id;
+INSERT INTO item(`name`,`description`,`price`,`item_meal`,`primary_ingredient`) VALUES (?,?,?,?,?)/*From insert item form*/ 
+/*After insert will grab text names of item_meal and primary_ingredient*/
+SELECT i.id, i.name, i.price, i.description, m.name AS item_meal, pi.name AS primary_ingredient FROM item i 
+INNER JOIN meal m ON i.item_meal = m.id INNER JOIN primary_ingredient pi ON i.primary_ingredient = pi.id WHERE i.name = :itemNameFromPostData
+DELETE FROM item WHERE id=:idFromDeleteFunction
 
--- add an item
-INSERT INTO item (name, price, description) VALUES (:name_frm, :price_frm, :desc_frm, :item_meal_frm)
-INSERT INTO meals_table(name) VALUES (:item_meal_frm)
-INSERT INTO pi_table(name) VALUES (:primary_ingredient)
+/*Add items to menu*/
+/* Gets restaurant names and menus for dropdown*/
+SELECT menu.id, menu.restaurant_name, meal.name AS meal_name FROM menu INNER JOIN meal ON menu.menu_meal = meal.id
+/*Gets items for selected menu table*/
+SELECT item.id, item.name, item.price, item.description, primary_ingredient.name AS primary_ingredient, meal.name AS item_meal FROM menu_items mi 
+INNER JOIN menu ON mi.mid = menu.id INNER JOIN item ON mi.iid = item.id 
+INNER JOIN meal ON item.item_meal = meal.id 
+INNER JOIN primary_ingredient ON item.primary_ingredient = primary_ingredient.id 
+WHERE mi.mid =?;
+/*Gets all items for all items table*/
+SELECT i.id, i.name, i.price, i.description, m.name AS item_meal, pi.name AS primary_ingredient FROM item i 
+INNER JOIN meal m ON i.item_meal = m.id 
+INNER JOIN primary_ingredient pi ON i.primary_ingredient = pi.id
+/*Add selected item to menu*/
+INSERT IGNORE INTO menu_items(mid,iid)VALUES(?,?);
+/*Then selects the entry with text names*/
+SELECT i.id, i.name, i.price, i.description, m.name AS item_meal, pi.name AS primary_ingredient FROM item i 
+INNER JOIN meal m ON i.item_meal = m.id 
+INNER JOIN primary_ingredient pi ON i.primary_ingredient = pi.id WHERE i.id = ?;
+/*Removes entry from table*/
+DELETE FROM menu_items WHERE (`mid` = ?) and (`iid` = ?);
 
--- delete a item
-DELETE FROM item WHERE id = :item_ID_selected_from_item_table
-DELETE FROM meals_table WHERE id = :meal_ID_selected_deleted_item
-DELETE FROM pi_table WHERE id = :primary_ingredient_selected_deleted_item
+/*Add primary ingredient */
+SELECT * FROM primary_ingredient ORDER BY id ASC
+INSERT INTO primary_ingredient (name) VALUES (?)
+DELETE FROM primary_ingredient WHERE id = ?
 
--- update item from table 
-SELECT name, price, description, item_meal, primary_ingredient FROM item WHERE id = :id_item_from_table --Populate edit form
-UPDATE item SET name = :name_frm, price = :price_frm, description =:desc_frm, item_meal = :item_meal_frm  WHERE id = :id_item_from_table
---more than likely the update will be handled by the back end javascript
-
-
-/*Add to Menu Page*/
--- Propogate menu table display
-SELECT name, restaurant_name, type FROM menu
-
-SELECT menu.id, menu.restaurant_name, meal.name AS menu_meal FROM menu INNER JOIN meal ON menu.menu_meal = meal.id
-
-INSERT INTO meal(`name`) VALUES (:KeyFromFrontEnd) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)
-
-DELETE FROM menu WHERE id= :menu_ID_selected_deleted_menu
-
-SELECT restaurant_name, item_meal FROM menus WHERE id = :menu_id_from_table --Populate edit form
-
-UPDATE meal SET name=? WHERE id = ? --Need to update individually 
-UPDATE menu SET restaurant_name=? WHERE id = ?
-
-/*Add to Add item to Menu Page*/
---Propogate menu select dropdown
-SELECT restaurant_name, meal_type FROM menu
-
---Selects menu id from  menu name
-SELECT id FROM menu WHERE :menu_id_is_equal_to_name_in_drop_down
-
---Propogate item table
-SELECT name, price, description, item_meal, primary_ingredient from item
-
-SELECT id from item where :item_id_is_equal_to_name_in_drop_down
-
-INSERT INTO menu_item (mid, iid) VALUES (:menu_id_from_dropdown, :item_id_from_dropdown)
-
-/*Display a Selected Menu*/
---Propogate menu select dropdown
-SELECT name FROM menu
-
---Selects menu id from menu name
-SELECT id FROM menu WHERE :menu_id_is_equal_to_name_in_drop_down
-
--- Get item from selected menu
-SELECT name, price, description
-FROM menus INNER JOIN menu_item ON  menu.id= mid
-INNER JOIN item ON item.id = iid
+/*Add meal */
+SELECT * FROM meal ORDER BY id ASC
+INSERT INTO meal (name) VALUES (?)
+DELETE FROM meal WHERE id = ?
